2547054d557d1cf034560c8da6a4cda9cdf20c02
Jonas Grimfelt <grimen@gmail.com>
2013-01-05 00:53:54 +0100

    Init.

---
+++ test/client.js
@@ -0,0 +1,49 @@
+var mongodb = require('mongodb');
+
+module.exports = {
+  get: function(db, type, id, callback) {
+    db = db.replace('.', '_');
+
+    var client = new mongodb.Db(db, new require('mongodb').Server('localhost', 27017), {safe: false});
+
+    client.open(function(err, db) {
+      db.collection(type, function(err, collection) {
+        collection.findOne({_id: id}, function(err, doc) {
+          db.close();
+          callback(err, doc);
+        });
+      });
+    });
+  },
+
+  set: function(db, type, id, data, callback) {
+    db = db.replace('.', '_');
+
+    var client = new mongodb.Db(db, new mongodb.Server('localhost', 27017), {safe: false});
+
+    client.open(function(err, db) {
+      db.collection(type, function(err, collection) {
+        data._id = id;
+        collection.update({_id:id}, data, {upsert: true, safe: true}, function(err, count) {
+          db.close();
+          callback(err, count);
+        });
+      });
+    });
+  },
+
+  del: function(db, type, id, callback) {
+    db = db.replace('.', '_');
+
+    var client = new mongodb.Db(db, new mongodb.Server('localhost', 27017), {safe: false});
+
+    client.open(function(err, db) {
+      db.collection(type, function(err, collection) {
+        collection.remove({_id:id}, {safe: true}, function(err, doc) {
+          db.close();
+          callback(err, doc);
+        });
+      });
+    });
+  }
+};
